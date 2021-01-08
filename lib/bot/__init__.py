cf9349278f6953144ae7c7522967a279555d5e50
TempusOwl <Olympus@binarychronos.com>
2021-01-08 02:14:13 -0500

    Update 0.0.5

--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,8 +1,12 @@
 from datetime import datetime
-from discord import Intents
-from discord import Embed, File
+
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
+from discord import Embed, File, Intents
 from discord.ext.commands import Bot as BotBase
+from discord.ext.commands import CommandNotFound
+
+from ..db import db
 
 PREFIX = "+"
 OWNER_IDS = [134118092082118657]
@@ -14,6 +18,8 @@ class Bot(BotBase):
         self.ready = False
         self.guild = None
         self.scheduler = AsyncIOScheduler()
+
+        db.autosave(self.scheduler)
 
         super().__init__(
             command_prefix=PREFIX,
@@ -30,35 +36,61 @@ class Bot(BotBase):
         print("running bot...")
         super().run(self.TOKEN, reconnect=True)
 
+    async def print_message(self):
+        channel = self.get_channel(696812072918581348)
+        await channel.send("I am a timer")
+
+    async def rules_reminder(self):
+        channel = self.get_channel(696812072918581348)
+        await channel.send("Remember to adhere to the rules!")
+
     async def on_connect(self):
         print("bot connected")
 
     async def on_disconnect(self):
         print("bot disconnected")
 
+    async def on_error(self, err, *args, **kwargs):
+        if err == "on_command_error":
+            await args[0].send("Something went wrong.")
+
+        channel = self.get_channel(696812072918581348)
+        await channel.send("An error occured.")
+        raise
+
+    async def on_command_error(self, ctx, exc):
+        if isinstance(exc, CommandNotFound):
+            pass
+
+        else:
+            raise exc.original
+
     async def on_ready(self):
         if not self.ready:
             self.ready = True
             self.guild = self.get_guild(663234840530780170)
-            print("bot ready")
+            self.scheduler.add_job(self.rules_reminder, CronTrigger(
+                day_of_week=0, hour=12, minute=0, second=0))
+            self.scheduler.start()
 
             channel = self.get_channel(796934983049543691)
-            await channel.send("Now Online!")
+            await channel.send("Now online!")
 
-            embed = Embed(title="Now Online!",
-                          description="Citadel Is Now Online.", color=0xFF0000, timestamp=datetime.utcnow())
-            fields = [("Name", "Value", True),
-                      ("and 2", "this field cool", True),
-                      ("A non-inline field", "on its own row", False)]
-            for name, value, inline in fields:
-                embed.add_field(name=name, value=value, inline=inline)
-            embed.set_author(name="Citadel", icon_url=self.guild.icon_url)
-            embed.set_footer(text="This is a footer!")
-            embed.set_thumbnail(url=self.guild.icon_url)
-            embed.set_image(url=self.guild.icon_url)
-            await channel.send(embed=embed)
+            # embed = Embed(title="Now online!", description="Citadel is now online.",
+            # 			  colour=0xFF0000, timestamp=datetime.utcnow())
+            # fields = [("Name", "Value", True),
+            # 		  ("Another field", "This field is next to the other one.", True),
+            # 		  ("A non-inline field", "This field will appear on it's own row.", False)]
+            # for name, value, inline in fields:
+            # 	embed.add_field(name=name, value=value, inline=inline)
+            # embed.set_author(name="Citadel", icon_url=self.guild.icon_url)
+            # embed.set_footer(text="This is a footer!")
+            # await channel.send(embed=embed)
 
-            await channel.send(file=File("./data/images/Mtvoq7M.jpg"))
+            # await channel.send(file=File("./data/images/Mtvoq7M.jpg"))
+
+            print("bot ready")
+
         else:
             print("bot reconnected")
 
